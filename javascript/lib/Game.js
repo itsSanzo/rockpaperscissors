96d912df7dacdd7222b91add83ed05add14946c5
itsmurasaki <itsmurasaki@gmail.com>
2014-06-05 20:11:40 +0100

    works if 1 of the 2 players wins

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -3,6 +3,21 @@ function Game(player1, player2) {
 	this.player2 = player2;
 };
 
+Game.prototype.PAIRS = {
+  rock: { scissors: "beats" },
+  paper: { rock: "beats" },
+  scissors: { paper: "beats" }
+}
+
 Game.prototype.winner = function() {
-  return this.player1;
+  if(this.victory(this.player1.pick, this.player2.pick)) {
+    return this.player1;
+  }
+  else {
+    return this.player2;
+  }
 };
+
+Game.prototype.victory = function(pick, opponentPick) {
+  return this.PAIRS[pick][opponentPick];
+}
