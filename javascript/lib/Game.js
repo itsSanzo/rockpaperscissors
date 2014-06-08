64cf3255bc668acd028e91fe9bf53e379c69dc48
itsmurasaki <itsmurasaki@gmail.com>
2014-06-08 18:55:15 +0100

    added web interface

--- javascript/lib/Game.js
+++ javascript/lib/Game.js
@@ -1,7 +1,7 @@
 function Game(player1, player2) {
 	this.player1 = player1;
 	this.player2 = player2;
-};
+}
 
 Game.prototype.PAIRS = {
   rock: { scissors: "crushes", lizard: "squashes" },
@@ -20,7 +20,25 @@ Game.prototype.winner = function() {
   else {
     return this.player2;
   }
-};
+}
+
+Game.prototype.loser = function () {
+  return (this.winner() === this.player1 ? this.player2 : this.player1);
+}
+
+Game.prototype.winningMessage = function() {
+  var message;
+
+  if(this.winner()) {
+    message = [this.winner().name,
+    this.victory(this.winner().pick, this.loser().pick),
+    this.loser().name].join(' ');
+  } else {
+    message = 'Draw!';
+  }
+
+  return message
+}
 
 Game.prototype.victory = function(pick, opponentPick) {
   return this.PAIRS[pick][opponentPick];
@@ -29,3 +47,6 @@ Game.prototype.victory = function(pick, opponentPick) {
 Game.prototype.draw = function() {
   return this.player1.pick === this.player2.pick;
 }
+
+
+
